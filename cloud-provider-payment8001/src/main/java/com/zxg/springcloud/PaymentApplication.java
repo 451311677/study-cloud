03a913edaa81d5cc21d40637989b7d472175e170
zxg <451311677@qq.com>
2021-02-18 15:06:01 +0800

    支付模块构建

---
+++ cloud-provider-payment8001/src/main/java/com/zxg/springcloud/PaymentApplication.java
@@ -0,0 +1,16 @@
+package com.zxg.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaogang.zhang
+ * @date 2021/2/17
+ * @description 主启动类
+ */
+@SpringBootApplication
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/zxg/springcloud/controller/PaymentController.java
@@ -0,0 +1,49 @@
+package com.zxg.springcloud.controller;
+
+import com.zxg.springcloud.entities.CommonResult;
+import com.zxg.springcloud.entities.Payment;
+import com.zxg.springcloud.serivice.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * @author xiaogang.zhang
+ * @date 2021/2/18
+ * @description
+ */
+@RestController
+@Slf4j
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping("/create")
+    public CommonResult<?> create(Payment payment) {
+//        Payment payment = new Payment();
+//        payment.setSerial("123");
+        int result = paymentService.create(payment);
+        log.info("***插入结果: " + result);
+
+        if (result > 0) {
+            return new CommonResult<Integer>(200, "插入数据库成功", result);
+        } else {
+            return new CommonResult<Object>(444, "插入数据库失败", null);
+        }
+    }
+
+    @GetMapping("/get/{id}")
+    public CommonResult<?> getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*** 查询结果：" + payment);
+
+        if (payment != null) {
+            return new CommonResult<Payment>(200, "查询成功", payment);
+        } else {
+            return new CommonResult<Object>(444, "，没有对应记录，id=" + id, null);
+        }
+    }
+}
