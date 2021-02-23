5693c14f203c655f9cbfbdf8322aa9b475ebebdf
zxg <451311677@qq.com>
2021-02-24 02:00:54 +0800

    hystrix订单模块 服务降级，兜底函数，全局通用处理函数

--- cloud-consumer-feign-hystrix-order81/src/main/java/com/zxg/springcloud/OrderHystrixApp81.java
+++ cloud-consumer-feign-hystrix-order81/src/main/java/com/zxg/springcloud/OrderHystrixApp81.java
@@ -3,6 +3,7 @@ package com.zxg.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 //激活Feign
 @EnableFeignClients
+@EnableHystrix
 public class OrderHystrixApp81 {
     public static void main(String[] args) {
         SpringApplication.run(OrderHystrixApp81.class,args);
--- cloud-consumer-feign-hystrix-order81/src/main/java/com/zxg/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order81/src/main/java/com/zxg/springcloud/controller/OrderHystrixController.java
@@ -1,5 +1,8 @@
 package com.zxg.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,18 +20,55 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 @RequestMapping("/consumer")
+//定义默认通用的fallback函数
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHystrixController {
 
     @Resource
     private PaymentHystrixService paymentHystrixService;
 
+
+    /**
+     * 默认的兜底函数
+     * @param id
+     * @return
+     */
+    @HystrixCommand
     @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfoOK(@PathVariable("id") Integer id){
+        int a = 10/0;
         return paymentHystrixService.paymentInfoOK(id);
     }
 
+
+    /**
+     * 消费者端 定义兜底处理fallback,
+     * @param id
+     * @return
+     */
     @GetMapping("/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            //设置自身调用超时时间的峰值,峰值内可以正常运行,  超过了需要有兜底的方法处理,做服务降级fallback
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000")
+    })
     public String paymentInfoError(@PathVariable("id") Integer id){
         return paymentHystrixService.paymentInfoError(id);
+    }
+
+    /**
+     * 超时/报错 处理方法
+     * @param id
+     * @return
+     */
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "消费者81 .。。。。。。。。。。";
+    }
+
+    /**
+     * 通用的默认处理方法
+     * @return
+     */
+    public String payment_Global_FallbackMethod(){
+        return "Global 异常处理信息，请稍后再试";
     }
 }
