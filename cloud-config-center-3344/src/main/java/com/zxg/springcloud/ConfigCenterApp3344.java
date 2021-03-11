27cef7732e116e9eb71fe6d555e299c2da135599
zxg <451311677@qq.com>
2021-03-11 19:52:10 +0800

    配置中心服务端

---
+++ cloud-config-center-3344/src/main/java/com/zxg/springcloud/ConfigCenterApp3344.java
@@ -0,0 +1,18 @@
+package com.zxg.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author xiaogang.zhang
+ * @date 2021/3/10
+ * @description 配置中心
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterApp3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterApp3344.class,args);
+    }
+}
