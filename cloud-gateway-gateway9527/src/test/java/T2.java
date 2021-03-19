40bcf46ed4799b48ca74aa9a1dce35b23f3cb769
zxg <451311677@qq.com>
2021-03-19 10:00:23 +0800

    gateWay  Test

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author xiaogang.zhang
+ * @date 2021/3/8
+ * @description 测试类
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
