99cca0de0dae9f84991f922d0f9a8fa7e3d73852
zxg <451311677@qq.com>
2021-03-19 09:55:22 +0800

    Nacos支付服务 生产者9002，9002搭建

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/zxg/springcloud/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.zxg.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiaogang.zhang
+ * @date 2021/3/18
+ * @description
+ */
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id){
+        return "nacos registry,serverPort: "+serverPort+"  ,  id: "+id;
+    }
+
+}
